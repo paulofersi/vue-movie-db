fb1b95e685997261de2fc4c050531e16aa976a9c
PauloFersi <80499561+trovap@users.noreply.github.com>
2022-10-31 23:14:50 -0300

    Final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,156 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <h3>Aqui você pode procurar os melhores filmes e séries para uma pesquisa rápida</h3>
+    </div>
+
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="Digite o nome do filme ou série" v-model="search" />
+      <input type="submit" value="Procurar" />
+    </form>
+
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue';
+import env from '@/env.js'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup () {
+    const search = ref("");
+    const movies = ref([]);
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          });
+      }
+    }
+    return {
+      search,
+      movies,
+      SearchMovies
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  h3{
+    color: white;
+    text-align: center;
+  }
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+    margin: 0px 16px;
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+      &[type="text"] {
+        width: 100%;
+        color: #000000;
+        background-color: #94eb42;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+        &::placeholder {
+          color: #000000;
+        }
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+      &[type="submit"] {
+        width: 100%;
+        max-width: 300px;
+        background-color: #125e3a;
+        padding: 16px;
+        border-radius: 8px;
+        color: #FFF;
+        font-size: 20px;
+        text-transform: uppercase;
+        transition: 0.4s;
+        &:active {
+          background-color: #3B8070;
+        }
+      }
+    }
+  }
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+    .movie {
+      max-width: 45%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        .product-image {
+          position: relative;
+          display: block;
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #b3ff00;
+            color: #000000;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+        .detail {
+          background-color: #125e3a;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+          .year {
+            color: #AAA;
+            font-size: 14px;
+          }
+          h3 {
+            color: #FFF;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
